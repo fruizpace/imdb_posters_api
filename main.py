eaa1c733226d149b5551a2c53cdfc698fe3df85e
Fiorella Ruiz-Pace <fruizpace@gmail.com>
2020-12-19 22:40:17 +0100

    Refactorización de consulta API omdb

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import requests
 
-# Buscar una película por el título
-pregunta = input('Título de la película: ')
 API_KEY =  56568558
-# construimos la direccion para solicitar la info de la peli
-direccion = "http://www.omdbapi.com/?apikey={}&s={}".format(API_KEY, pregunta)
+url_template = "http://www.omdbapi.com/?apikey={}&{}={}"
 
-respuesta = requests.get(direccion)
-
-if respuesta.status_code == 200: 
-    datos = respuesta.json() # Datos es un diccionario!
-    if datos['Response'] == "False":
-        print(datos["Error"])
+def peticion(url):
+    respuesta = requests.get(url)
+    if respuesta.status_code == 200: # si ha ido bien...
+        datos = respuesta.json() # guarda la respuesta
+        if datos['Response'] == "False": # si la respuesta tiene resultado negativo
+            return datos["Error"] # string
+        else: # si la respuesta tiene resultado positivo (o sea hay info de pelis)
+            return datos
     else:
-        primera_peli = datos['Search'][0] # 1er elemento de la lista de pelis que coinciden con el título
-        clave = primera_peli['imdbID'] # identificador de la primera peli
-        
-        otra_direccion = "http://www.omdbapi.com/?apikey={}&i={}".format(API_KEY, clave)
-        nueva_respuesta = requests.get(otra_direccion)
-        if nueva_respuesta.status_code == 200:
-            datos = nueva_respuesta.json()
-            if datos['Response'] == "False":
-                print(datos["Error"])
-            else:
-                titulo = datos['Title']
-                agno = datos['Year']
-                director = datos['Director']
-                print("La peli {}, del año {}, fue dirigida por {}").format(titulo, agno, director)
-        else:
-            print('Error en consulta por id:', nueva_respuesta.status_code)
-else:
-    print("Error en búisqueda", respuesta.status_code)
+        return respuesta.status_code #string
+
+pregunta = input('Título de la película: ')
+
+# Petición 1: para obtener identificador de la peli
+respuesta = peticion(url_template.format(API_KEY,'s', pregunta)) # 's' = search
+if isinstance(respuesta, str):
+    print("Error en consulta: ", respuesta)
+else: # si da un diccionario
+    primera_peli = respuesta['Search'][0] # 1er elemento de la lista de pelis que coinciden con el título
+    clave = primera_peli['imdbID'] # identificador de la primera peli
+    # Petición 2: con el id ahora puedo acceder a más info de la peli
+    respuesta = peticion(url_template.format(API_KEY, 'i', clave))
+    if isinstance(respuesta, str):
+        print(respuesta)
+    else:
+        titulo = respuesta['Title']
+        agno = respuesta['Year']
+        director = respuesta['Director']
+        print("La película {} ({}) fue dirigida por {}.".format(titulo, agno, director))
--- probatina.py
+++ probatina.py
@@ -10,3 +10,38 @@ if respuesta.status_code == 200: # los tipos de error en https://developer.mozil
     datos = respuesta.json() # Guardamos la info en formato json. Datos es un diccionario!
 else:
     print("Se ha producido un error", respuesta.status_code)
+
+
+####
+
+# Buscar una película por el título
+pregunta = input('Título de la película: ')
+API_KEY =  56568558
+# construimos la direccion para solicitar la info de la peli
+direccion = "http://www.omdbapi.com/?apikey={}&s={}".format(API_KEY, pregunta)
+
+respuesta = requests.get(direccion)
+
+if respuesta.status_code == 200: 
+    datos = respuesta.json() # Datos es un diccionario!
+    if datos['Response'] == "False":
+        print(datos["Error"])
+    else:
+        primera_peli = datos['Search'][0] # 1er elemento de la lista de pelis que coinciden con el título
+        clave = primera_peli['imdbID'] # identificador de la primera peli
+        
+        otra_direccion = "http://www.omdbapi.com/?apikey={}&i={}".format(API_KEY, clave)
+        nueva_respuesta = requests.get(otra_direccion)
+        if nueva_respuesta.status_code == 200:
+            datos = nueva_respuesta.json()
+            if datos['Response'] == "False":
+                print(datos["Error"])
+            else:
+                titulo = datos['Title']
+                agno = datos['Year']
+                director = datos['Director']
+                print("La película {} ({}) fue dirigida por {}.".format(titulo, agno, director))
+        else:
+            print('Error en consulta por id:', nueva_respuesta.status_code)
+else:
+    print("Error en búisqueda", respuesta.status_code)
